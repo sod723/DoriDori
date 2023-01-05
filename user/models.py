6cca832ceb6b3d44500006a5fdda2694a1250c64
sod723 <sod723@naver.com>
2023-01-06 04:09:32 +0900

    log in

--- user/models.py
+++ user/models.py
@@ -8,6 +8,7 @@ class User(AbstractBaseUser):
     # user_name = models.CharField(max_length=24)
     # user_email = models.EmailField(max_length=128, unique=True)
     # user_phone = models.CharField(max_length=13)
+
     # #is_passenger = models.BooleanField(default=True)
     #
     # USERNAME_FIELD = 'user_email'
@@ -16,12 +17,14 @@ class User(AbstractBaseUser):
     # class Meta:
     #     db_table = "User"
 
-    phone = models.CharField(max_length=24, unique=True)
+    profile_image = models.TextField()  # 프로필 이미지
+    nickname = models.CharField(max_length=24, unique=True)
     name = models.CharField(max_length=24)
     email = models.EmailField(unique=True)
-    is_passenger = models.TextField()  # 프로필 이미지
+    phonenum = models.CharField(max_length=24)
+    # is_passenger = models.BooleanField()
 
-    USERNAME_FIELD = 'phone'
+    USERNAME_FIELD = 'nickname'
 
     class Meta:
         db_table = "User"
