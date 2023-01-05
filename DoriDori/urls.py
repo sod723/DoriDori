adc60b3971e8906155687d82f23c16eb70cfbb4a
sod723 <sod723@naver.com>
2023-01-06 04:09:32 +0900

    log in

--- DoriDori/urls.py
+++ DoriDori/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include
+from django.urls import path, include
+# from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import Main
@@ -24,6 +24,7 @@ urlpatterns = [
                   path('', Main.as_view()),
                   path('', include('board.urls')),
                   path('', include('content.urls')),
-                  path('', include('user.urls')),
+                  path('user/', include('user.urls')),
 
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+              ] \
+              # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- user/urls.py
+++ user/urls.py
@@ -4,8 +4,9 @@ from .views import Join, Login, LogOut
 
 urlpatterns = [
     #path('login', views.login, name="login"),
-    path('join/', Join.as_view(), name='join'),
-    path('login/', Login.as_view(), name='login'),
-    path('join', Join.as_view(), name='join'),
-    path('login', Login.as_view(), name='login'),
+    path('join', Join.as_view()),
+    path('login', Login.as_view()),
+    path('logout', LogOut.as_view()),
+    # path('join', Join.as_view(), name='join'),
+    # path('login', Login.as_view(), name='login'),
 ]
