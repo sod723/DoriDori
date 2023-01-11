31942e7c60336b9641c2d0246c4ebb6889bf92da
sod723 <sod723@naver.com>
2023-01-12 06:28:05 +0900

    login 수정

--- content/urls.py
+++ content/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'content'
+
 urlpatterns = [
-    path('map', views.map, name="map"),
+    path('map/', views.map, name="map"),
+    path('userRoute', views.userRoute, name="userRoute"),
+    path('driverRoute', views.driverRoute, name="driverRoute"),
 ]
--- user/urls.py
+++ user/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
-from .views import Join, Login, LogOut
+
+app_name = 'user'
+
 
 urlpatterns = [
-    #path('login', views.login, name="login"),
-    path('join', Join.as_view()),
-    path('login', Login.as_view()),
-    path('logout', LogOut.as_view()),
-    # path('join', Join.as_view(), name='join'),
-    # path('login', Login.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
