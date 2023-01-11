fa1f6b06e25ada95eb6a386018fe2d66a984b9e3
sod723 <sod723@naver.com>
2023-01-11 15:21:15 +0900

    sign up type

--- board/models.py
+++ board/models.py
@@ -1,4 +1,6 @@
+
 from django.db import models
+
 
 # Create your models here.
 class Board(models.Model):
@@ -6,6 +8,7 @@ class Board(models.Model):
     b_title = models.CharField(db_column='b_title',max_length=255, blank=True, null=True)
     b_note = models.CharField(db_column='b_note',max_length=4096, blank=True, null=True)
     b_writer = models.CharField(db_column='b_writer',max_length=50, blank=True, null=True)
+    # b_writer = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True)
     parent_no = models.IntegerField(db_column='parent_no',blank=True, null=True)
     b_count = models.IntegerField(db_column='b_count',blank=True, null=True)
     b_date = models.DateTimeField(blank=True, null=True)
