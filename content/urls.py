7c91481e379169c0a7e8f729363636369939cba7
JungHun98 <j7papa@naver.com>
2023-01-12 15:57:14 +0900

    set route on map

--- content/urls.py
+++ content/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     path('map', views.map, name="map"),
-    path('userRoute', views.userRoute, name="userRoute"),
-    path('driverRoute', views.driverRoute, name="driverRoute"),
 ]
