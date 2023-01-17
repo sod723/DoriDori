28fdb88392dd67bcfc480d860d31bbd44fe2d843
sod723 <sod723@naver.com>
2023-01-17 12:30:29 +0900

    도로명 주소 입력

--- content/urls.py
+++ content/urls.py
@@ -5,6 +5,10 @@ app_name = 'content'
 
 urlpatterns = [
     path('map/', views.map, name="map"),
+    path('getUsrLatLng/', views.getUsrLatLng, name='getApi'),
+    path('SetSpot', views.GetSpotPoint, name='getspotpoint'),
+    # path('pathfinder', views.PathFinder, name='pathfinder'),
+    # path('saferoute', views.saferoute, name='saferoute'),
     # path('userRoute', views.userRoute, name="userRoute"),
     # path('driverRoute', views.driverRoute, name="driverRoute"),
 ]
